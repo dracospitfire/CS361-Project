c1511d0a029dab8ea41774fc172d66fe1c07bea9
Austin Flores <austin3flores@gmail.com>
2025-04-27 15:56:03 -0700

    added production check to only open ports in produciton

--- Pokemon-Professor/vite.config.js
+++ Pokemon-Professor/vite.config.js
@@ -15,6 +15,6 @@ export default defineConfig({
   server: {
     // Use VITE_PORT from your .env, or default to a port if not specified
     port: parseInt(process.env.VITE_PORT, 10) || 3000,
-    host: '0.0.0.0',
+    host: process.env.NODE_ENV === 'production' ? '0.0.0.0' : 'localhost',
   }
 })
